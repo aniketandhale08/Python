84c6d7111baf5217827021b41f44404018eb1110
Aniket Andhale <99685171+aniketandhale08@users.noreply.github.com>
2024-10-17 01:50:08 +0530

    Add files via upload

---
+++ addition_division.py
@@ -0,0 +1,18 @@
+# Addition
+num_1 = float(input("Enter the first num:"))
+num_2 = float(input("Enter the second num:"))
+
+addition = num_1 + num_2
+print(f"The sum of {num_1} and {num_2} is {addition}")
+
+
+# Division
+
+num_3 = float(input("Enter the first num:"))
+num_4 = float(input("Enter the second num:"))
+
+if num_4 == 0:
+    print("Can't divide by zero")
+else:
+    division = num_3/num_4
+    print(f"The divison of {num_3} and {num_4} is {division}")
