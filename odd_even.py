b4f6a2935eb155013c8eb322c1cd7a9e463ae42c
Aniket Andhale <99685171+aniketandhale08@users.noreply.github.com>
2024-10-22 02:43:20 +0530

    Add files via upload

---
+++ odd_even.py
@@ -0,0 +1,17 @@
+num = int(input("Enter the num to check: "))
+if num % 2  == 0:
+    print("Even")
+else :
+    print("Odd")
+    
+    
+# method 2
+
+num = int(input("Enter the num to check: "))
+if (num // 2) * 2  == num:
+    print("Even")
+else :
+    print("Odd")
+
+
+
