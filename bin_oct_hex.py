6e6f8780536ac837fa081cab7e031ae326821128
Aniket Andhale <99685171+aniketandhale08@users.noreply.github.com>
2024-12-11 02:19:45 +0530

    Add files via upload

---
+++ bin_oct_hex.py
@@ -0,0 +1,13 @@
+num= int(input("Enter the num: "))
+
+print("Your original num is: ", num)
+print(num ,"in binary is" ,bin(num))
+print(num ,"in oct is" ,oct(num))
+print(num ,"in hex is" ,hex(num))
+
+"""
+Binary: Divide the decimal number by 2.
+Octal: Divide the decimal number by 8.
+Hexadecimal: Divide the decimal number by 16.
+"""
+
