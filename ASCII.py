e45579d747a13d210fac80619412ef6a8ef28fa3
Aniket Andhale <99685171+aniketandhale08@users.noreply.github.com>
2024-12-11 02:19:09 +0530

    Add files via upload

---
+++ ASCII.py
@@ -0,0 +1,11 @@
+"""
+ASCII (American Standard Code for Information Interchange) is a system that assigns a unique numerical value to characters, 
+symbols, and control codes in computers. It is widely used in programming and text representation.
+ASCII values range from 0 to 127.
+"""
+
+print(ord(str("A")))
+
+
+char = str(input("Enter the character: "))
+print(f"The ASCII value of {char} is: ",ord(char))
