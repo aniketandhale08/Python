435ab037b8aa84f7c71689a9d179cd715e480793
Aniket Andhale <99685171+aniketandhale08@users.noreply.github.com>
2024-12-20 01:11:55 +0530

    Add files via upload

---
+++ split.py
@@ -0,0 +1,30 @@
+# my_str = "welcome to the jungle"
+# new_str = my_str.split()
+# print(new_str)
+
+
+
+# Split the string, using comma, followed by a space, as a separator
+# my_str = "hello, my name is Aniket, I am 21 years old"
+# new_str = my_str.split(", ")
+# print(new_str)
+
+
+
+# Use a hash character as a separator
+# my_str = "my#name#is#aniket"
+# new_str = my_str.split("#")
+# print(new_str)
+
+
+
+# Split the string into a list with max 2 items
+my_str = "my#name#is#aniket"
+# setting the maxsplit parameter to 1, will return a list with 2 elements!
+new_str = my_str.split("#", 1)
+print(new_str)
+
+
+
+
+
