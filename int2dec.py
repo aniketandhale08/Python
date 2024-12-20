ca29167c5e160d20b1840d69c1411e4b485c3458
Aniket Andhale <99685171+aniketandhale08@users.noreply.github.com>
2024-12-20 23:33:38 +0530

    Add files via upload

---
+++ int2dec.py
@@ -0,0 +1,21 @@
+# deciaml is particularly useful when you need high precision in calculations, such as in financial or scientific applications.
+
+"""
+import decimal
+
+num = int(input("Enter the num: "))
+
+print(decimal.Decimal(num))
+print(type(decimal.Decimal(num)))
+
+ """
+
+
+
+# string of int to the decimal
+
+import decimal
+str = "1234"
+
+print(decimal.Decimal(str))
+print(type(decimal.Decimal(str)))
