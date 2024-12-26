5221c17dc967eb1c0d0fdeadeb84dee16ddb924e
Aniket Andhale <99685171+aniketandhale08@users.noreply.github.com>
2024-12-26 18:37:19 +0530

    Add files via upload

---
+++ args.py
@@ -0,0 +1,17 @@
+"""
+By using *args You can now pass multiple arguments to the function, and it will treat them as a tuple.
+you cannot directly implement *args without a function, 
+because the purpose of *args is specifically to allow a function to accept a variable number of arguments.
+"""
+
+
+# def average(*args):
+#     avg = sum(args) / len(args)
+#     return avg
+
+# average1 = average(10, 20, 30)  
+# average2 = average(10, 20, 30, 40, 50)   
+# print(average1)  
+# print(average2)  
+
+
