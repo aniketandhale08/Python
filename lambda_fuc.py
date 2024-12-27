65dc7a16c60c90d15543b9e140dd0fda0812bbb4
Aniket Andhale <99685171+aniketandhale08@users.noreply.github.com>
2024-12-27 23:55:03 +0530

    Add files via upload

---
+++ lambda_fuc.py
@@ -0,0 +1,15 @@
+# addition = lambda a,b,c : a+b+c
+# num1 = addition(10,20,30)
+# print(num1)
+
+
+# method 2
+
+# print((lambda a,b,c : a+b+c) (10,20,30))
+
+
+
+# example
+
+square = lambda x: x * x
+print(square(5)) 
