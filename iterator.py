3cf8c63415514cab2854d7463a06fb69446c0a59
Aniket Andhale <99685171+aniketandhale08@users.noreply.github.com>
2025-01-01 00:54:42 +0530

    Add files via upload

---
+++ iterator.py
@@ -0,0 +1,35 @@
+# my_list = [1,2,3,4,5,6,7,8]
+
+# it = iter(my_list)
+
+# while True:
+#     try:
+#         print(next(it))
+#     except StopIteration:
+#         break
+
+
+
+
+# Program to print elements of a list using an iterator
+
+"""
+my_list1 = [1,2,3,4,5,6,7]
+it = iter(my_list1)
+# print(next(it)) 
+for i in it:
+    print(i)
+
+"""
+
+
+# Program to iterate over a string using an iterator
+
+"""
+my_str = "My name is aniket"
+it = iter(my_str)
+# print(next(it))
+for i in it:
+    print(i, end="")
+
+"""
