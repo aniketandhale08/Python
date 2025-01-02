b03e191769f7411cd9a322037d6b00f532b1a86f
Aniket Andhale <99685171+aniketandhale08@users.noreply.github.com>
2025-01-03 04:47:10 +0530

    Add files via upload

---
+++ else_loop.py
@@ -0,0 +1,43 @@
+# for i in range(5):
+#     print(i)
+# else:
+#     print("Loop completed without a break")
+
+
+
+
+
+
+
+# for i in range(5):
+#     if i == 3:
+#         print("Breaking the loop")
+#         break
+#     print(i)
+# else:
+#     print("Loop completed without a break")  # This will not execute
+
+
+
+
+
+# x = 0
+# while x < 3:
+#     print(x)
+#     x += 1
+# else:
+#     print("While loop completed without a break")
+
+
+
+
+
+x = 0
+while x < 5:
+    if x == 3:
+        print("Breaking the loop")
+        break
+    print(x)
+    x += 1
+else:
+    print("While loop completed without a break")  # This will not execute
