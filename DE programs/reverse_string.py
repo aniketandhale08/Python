0a2b9a7c377edfedc563aa3e47b1050ea32e69fd
Aniket Andhale <99685171+aniketandhale08@users.noreply.github.com>
2025-01-12 19:22:23 +0530

    Add files via upload

---
+++ DE programs/reverse_string.py
@@ -0,0 +1,13 @@
+# Write a Python program to reverse a given string.
+
+# input_string = input("Enter the string: ")
+# reverse_str= input_string[::-1]
+# print(reverse_str)
+
+
+
+def reverse_str(input_str):
+    return input_str[::-1]
+
+input_str = input("Enter the string: ")
+print(reverse_str(input_str))
